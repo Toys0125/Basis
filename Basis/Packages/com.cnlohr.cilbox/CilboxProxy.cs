217ad37965bc49946cba570ca4794f1af21e9cf5
Toys0125 <marcus.g.stange@gmail.com>
2026-09-14 22:40:31 +0000

    Measure 20-avatar Fox Moth burst initialization

--- Basis/Packages/com.cnlohr.cilbox/CilboxProxy.cs
+++ Basis/Packages/com.cnlohr.cilbox/CilboxProxy.cs
@@ -31,6 +31,7 @@ namespace Cilbox
 		private bool proxyWasSetup = false;
 		private bool proxyLoadInProgress = false;
 #if UNITY_EDITOR
+		private static readonly ProfilerMarker LivePlaytestStartMarker = new ProfilerMarker("CilboxProxy.Start.LivePlaytest");
 		private static readonly ProfilerMarker LivePlaytestUpdateMarker = new ProfilerMarker("CilboxProxy.Update.LivePlaytest");
 #endif
 
@@ -521,6 +522,9 @@ namespace Cilbox
 
 
 		void Start() {
+#if UNITY_EDITOR
+			using var marker = LivePlaytestStartMarker.Auto();
+#endif
 			RuntimeProxyLoad();
 
 			if( proxyWasSetup ) {
